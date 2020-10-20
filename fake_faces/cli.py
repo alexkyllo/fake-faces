9bf30b5bffd503c25993f164dd291fafe6a879e5
Alex Kyllo <alex.kyllo@gmail.com>
2020-10-20 16:34:54 -0700

    add makefile

--- fake_faces/cli.py
+++ fake_faces/cli.py
@@ -3,3 +3,19 @@
 import os
 import logging
 import click
+
+
+@click.group()
+def cli():
+    """The fake-faces command line interface."""
+
+
+def main():
+    """Program entry point"""
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    cli()
+
+
+if __name__ == "__main__":
+    main()
