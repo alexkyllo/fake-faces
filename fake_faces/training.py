c314c1960d02e9d73a96e2a69a6d0098fe0f10a9
Alex Kyllo <alex.kyllo@gmail.com>
2020-10-20 22:31:00 -0700

    add some tf imports

--- fake_faces/training.py
+++ fake_faces/training.py
@@ -1,1 +1,10 @@
 """training.py"""
+import click
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+
+
+@click.command()
+def train():
+    """Train the CNN model on training data and save it."""
