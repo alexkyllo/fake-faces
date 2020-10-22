e95e5111cee2dfcc8408e1fb6806e7aa26f51459
Alex Kyllo <alex.kyllo@gmail.com>
2020-10-22 08:41:47 -0700

    work on training code

--- fake_faces/training.py
+++ fake_faces/training.py
@@ -1,4 +1,6 @@
 """training.py"""
+import os
+import time
 import logging
 import click
 import tensorflow as tf
@@ -30,15 +32,30 @@ def make_generator(train=True):
     )
     return gen
 
+def make_model():
+    """Build a Keras sequential model"""
+    model = Sequential()
+    # TODO
+
 
 @click.command()
 @click.argument("input_path", type=click.Path(exists=True))
 @click.argument("output_path", type=click.Path())
-def train(input_path, model_path):
+@click.argument("epochs", type=click.INT)
+def train(input_path, model_path, epochs):
     """Train the CNN model on training data and save it."""
     logger = logging.getLogger(__name__)
     logger.info(
-        "GPUs available: %s", tf.config.experimental.list_physical_devices("GPU")
+        "GPUs available for training: %s", tf.config.experimental.list_physical_devices("GPU")
     )
-    gen = make_generator(train=True)
-    # TODO
+    train_gen = make_generator(train=True)
+    train = gen.flow_from_directory(input_path,
+                                    class_mode="binary",
+                                    batch_size=64,
+                                    target_size=(64, 64),
+                                    color_mode="grayscale")
+    model = make_model()
+    train_start = time.time()
+    model.fit_generator(train, epochs=epochs, steps_per_epoch=len(train))
+    train_end = time.time()
+    logger.info("Training completed in %.2f seconds", train_end - train_start)
