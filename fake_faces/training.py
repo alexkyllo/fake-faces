b362b15b50fe7c14cea8a25754cc18b16995c829
Alex Kyllo <alex.kyllo@gmail.com>
2020-10-22 10:19:08 -0700

    explicit keyword args to keras layers

--- fake_faces/training.py
+++ fake_faces/training.py
@@ -38,23 +38,33 @@ def make_generator(train=True):
 def make_model():
     """Build a CNN model using Keras Sequential API"""
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=SHAPE, activation="relu", padding="same"))
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            input_shape=SHAPE,
+            activation="relu",
+            padding="same",
+        )
+    )
     model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Dropout(rate=0.2))
 
-    model.add(Conv2D(64, (3, 3), activation="relu", padding="same"))
-    model.add(MaxPool2D((2, 2)))
-    model.add(Dropout(0.2))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.2))
 
-    model.add(Conv2D(128, (3, 3), activation="relu", padding="same"))
-    model.add(MaxPool2D((2, 2)))
-    model.add(Dropout(0.2))
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.2))
 
     model.add(Flatten())
 
-    model.add(Dense(128, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation="sigmoid"))
+    model.add(Dense(units=128, activation="relu"))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(units=1, activation="sigmoid"))
 
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
     return model
