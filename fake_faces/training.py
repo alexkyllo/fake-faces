6b85da2882a12a7b8dbb0c68255b3dee9333da30
Alex Kyllo <alex.kyllo@gmail.com>
2020-11-13 17:40:12 -0800

    add batchnorm model

--- fake_faces/training.py
+++ fake_faces/training.py
@@ -8,12 +8,20 @@ import click
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import (
+    Conv2D,
+    MaxPool2D,
+    Flatten,
+    Dense,
+    Dropout,
+    BatchNormalization,
+)
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 SHAPE = (128, 128)
 BATCH_SIZE = 64
 CLASS_MODE = "binary"
+
 
 def make_generator(train=True):
     """Create an ImageDataGenerator object to read images from the
@@ -33,14 +41,12 @@ def make_generator(train=True):
             "height_shift_range": 0.2,
             "horizontal_flip": True,
         }
-    gen = ImageDataGenerator(
-        **params,
-    )
+    gen = ImageDataGenerator(**params,)
     return gen
 
 
 def make_model(color_channels=1, weights_file=None):
-    """Build a CNN model using Keras Sequential API"""
+    """Build a baseline CNN model using Keras Sequential API"""
     # TODO: Try adding batch normalization
     # TODO: Try doubling up the Conv2D layers
     # TODO: Try training on RGB images instead of grayscale
@@ -80,6 +86,93 @@ def make_model(color_channels=1, weights_file=None):
     return model
 
 
+def make_model_batch_norm(color_channels=1, weights_file=None):
+    """Build a CNN model using Keras Sequential API, with BatchNormalization instead of Dropout"""
+    # TODO: Try adding batch normalization
+    # TODO: Try doubling up the Conv2D layers
+    # TODO: Try training on RGB images instead of grayscale
+    # TODO: Try Inception, Xception, ResNet, EfficientNet based architectures
+    model = Sequential()
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            input_shape=(*SHAPE, color_channels),
+            activation="relu",
+            padding="same",
+        )
+    )
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same"))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Flatten())
+
+    model.add(Dense(units=128, activation="relu"))
+    model.add(Dense(units=1, activation="sigmoid"))
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    if weights_file:
+        model.load_weights(weights_file)
+    return model
+
+
+def make_model_vgg(color_channels=1, weights_file=None):
+    """This model failed to learn the problem, accuracy was 50% after 9 epochs"""
+    model = Sequential()
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            input_shape=(*SHAPE, color_channels),
+            activation="relu",
+            padding="same",
+        )
+    )
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu", padding="same"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.2))
+
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same"))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same"))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.2))
+
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(
+        Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
+    )
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(rate=0.2))
+
+    model.add(Flatten())
+
+    model.add(Dense(units=128, activation="relu"))
+    model.add(Dropout(rate=0.5))
+    model.add(Dense(units=1, activation="sigmoid"))
+
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+    if weights_file:
+        model.load_weights(weights_file)
+    return model
+
+
 def check_gpu():
     logger = logging.getLogger(__name__)
     gpus = tf.config.experimental.list_physical_devices("GPU")
@@ -87,7 +180,15 @@ def check_gpu():
     return len(gpus) > 0
 
 
-def train_model(train_path, valid_path, model_dir, epochs, weights_path=None, color_mode="grayscale"):
+def train_model(
+    model,
+    train_path,
+    valid_path,
+    model_dir,
+    epochs,
+    weights_path=None,
+    color_mode="grayscale",
+):
     """Train the CNN model on training data and save it."""
     logger = logging.getLogger(__name__)
     check_gpu()
@@ -101,7 +202,9 @@ def train_model(train_path, valid_path, model_dir, epochs, weights_path=None, co
     train = train_gen.flow_from_directory(train_path, **flow_args)
     val_gen = make_generator(train=False)
     val = val_gen.flow_from_directory(valid_path, **flow_args)
-    model_path = os.path.join(model_dir, "model.{epoch:02d}-{val_loss:.2f}-{val_accuracy:.2f}.hdf5")
+    model_path = os.path.join(
+        model_dir, "model.{epoch:02d}-{val_loss:.2f}-{val_accuracy:.2f}.hdf5"
+    )
     log_path = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard = tf.keras.callbacks.TensorBoard(log_dir=log_path, histogram_freq=1)
     checkpoint = ModelCheckpoint(
@@ -111,8 +214,6 @@ def train_model(train_path, valid_path, model_dir, epochs, weights_path=None, co
         save_best_only=True,
         verbose=1,
     )
-    color_channels = 3 if color_mode == "rgb" else 1
-    model = make_model(color_channels=color_channels, weights_file=weights_path)
 
     if weights_path:
         initial_epoch = int(re.findall("model.([0-9]+)", weights_path)[0])
@@ -150,4 +251,5 @@ def train_model(train_path, valid_path, model_dir, epochs, weights_path=None, co
 )
 def train(train_path, valid_path, model_path, epochs, weights):
     """Train the model on images in TRAIN_PATH and validate on VALID_PATH for # EPOCHS"""
-    train_model(train_path, valid_path, model_path, epochs, weights)
+    model = make_model(weights_file=weights)
+    train_model(model, train_path, valid_path, model_path, epochs, weights)
