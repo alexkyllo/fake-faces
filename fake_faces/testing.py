3e48d2da96bb2c4a98ae0dd31ae5c024da9266cf
Alex Kyllo <alex.kyllo@gmail.com>
2020-11-17 21:55:02 -0800

    fix bug in make_confusion_matrix, add pydot to graphviz the CNN

--- fake_faces/testing.py
+++ fake_faces/testing.py
@@ -1,26 +1,25 @@
 """testing.py
 """
-from fake_faces.training import (
-    make_generator,
-    make_model,
+from fake_faces import (
     CLASS_MODE,
     BATCH_SIZE,
     SHAPE,
-    COLOR_MODE,
 )
+from fake_faces.models.model import make_generator
+from tensorflow.keras.models import load_model
 import numpy as np
 from sklearn import metrics
 
 
-def make_confusion_matrix(weights_file, test_path, threshold=0.5):
+def make_confusion_matrix(weights_file, test_path, threshold=0.5, color_mode="grayscale"):
     """Calculate metrics on test data"""
-    model = make_model(weights_file=weights_file)
+    model = load_model(weights_file)
     test_gen = make_generator(train=False)
     flow_args = dict(
         class_mode=CLASS_MODE,
         batch_size=BATCH_SIZE,
         target_size=SHAPE[0:2],
-        color_mode=COLOR_MODE,
+        color_mode=color_mode,
         shuffle=False,
     )
     test = test_gen.flow_from_directory(test_path, **flow_args)
