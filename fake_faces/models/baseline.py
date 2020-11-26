26568095c8a22906b2140164d658cf12966357d3
Alex Kyllo <alex.kyllo@gmail.com>
2020-11-25 23:23:00 -0800

    update baseline model to parameterize hyperparams

--- fake_faces/models/baseline.py
+++ fake_faces/models/baseline.py
@@ -10,13 +10,21 @@ from tensorflow.keras.layers import (
     Dropout,
     BatchNormalization,
 )
-
+from tensorflow.keras.optimizers import Adam
 from fake_faces.models.model import Model
 from fake_faces import SHAPE, BATCH_SIZE, CLASS_MODE
 
 
 class Baseline(Model):
-    def __init__(self, path, log_path, color_channels=1):
+    def __init__(
+        self,
+        path,
+        log_path,
+        color_channels=1,
+        maxpool_dropout_rate=0.2,
+        dense_dropout_rate=0.5,
+        optimizer=Adam(),
+    ):
         """constructor"""
         super().__init__(path, log_path, color_channels)
 
@@ -32,28 +40,28 @@ class Baseline(Model):
             )
         )
         model.add(MaxPool2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.2))
+        model.add(Dropout(maxpool_dropout_rate))
 
         model.add(
             Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same")
         )
         model.add(MaxPool2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.2))
+        model.add(Dropout(maxpool_dropout_rate))
 
         model.add(
             Conv2D(filters=128, kernel_size=(3, 3), activation="relu", padding="same")
         )
         model.add(MaxPool2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.2))
+        model.add(Dropout(maxpool_dropout_rate))
 
         model.add(Flatten())
 
         model.add(Dense(units=128, activation="relu"))
-        model.add(Dropout(rate=0.5))
+        model.add(Dropout(dense_dropout_rate))
         model.add(Dense(units=1, activation="sigmoid"))
 
         model.compile(
-            optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]
+            optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"]
         )
         if self.checkpoint:
             model.load_weights(self.checkpoint)
