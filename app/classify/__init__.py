3e808d628b29db3d709fe4c665ccceaa2c2baecd
JWyman482 <jwyman4821@gmail.com>
2020-12-14 01:50:28 -0800

    Refactor and cleanup

--- app/classify/__init__.py
+++ app/classify/__init__.py
@@ -2,26 +2,21 @@ import logging
 import azure.functions as func
 import json
 import tempfile
-from .predict import predict_image_from_url, predict_image_from_file
-
-# 
+from .predict import predict_image
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     image_url = req.params.get('img')
     logging.info('Image URL received: %s', image_url)
+    contents = image_url
+    
     if image_url == None:
-        logging.info(req.files.values())
-        for input_file in req.files.values():
-            # logging.info('In Loop!!')
-            # filename = input_file.filename
-            contents = input_file.stream.read()
-            # logging.info('Filename: %s' % filename)
 
-            image_contents = contents
-        # logging.info('Out of loop!')
-        results = predict_image_from_file(image_contents)
-    else:
-        results = predict_image_from_url(image_url)
+        # logging.info(req.files.values())
+
+        for input_file in req.files.values():
+            contents = input_file.stream.read()
+
+    results = predict_image(contents)
 
     # TODO: Set Access-Control-Allow-Origin header to hosted location of frontend
     headers = {
@@ -38,4 +33,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     jsond = json.dumps(resp)
     result = json.loads(jsond)
+
     return func.HttpResponse(json.dumps(result), headers = headers)
--- app/classify/predict.py
+++ app/classify/predict.py
@@ -3,75 +3,39 @@ Perform inference on a single image from a URL."""
 import os  # os in order to grab the correct path for the model
 import logging
 import urllib  # Used to convert from URL to an image file
-import requests as requests
-from io import BytesIO, StringIO  # Used to convert from URL to an image file
+from io import BytesIO  # Used to convert from URL to an image file
 import tensorflow as tf
 import tensorflow.keras.preprocessing.image as image
 from tensorflow.keras.models import load_model
 from PIL import Image  # Used to convert from URL to an image file
 
-# TODO: Refactor to combine load_image(url) and load_image_b(img_file)
-# TODO: Refactor to combine predict image methods
 
-def load_image(url):
-    """Open image bytes from a URL and return as a numpy array."""
-    logging.info("predict.py: Image URL received now: %s", url)
-    
-    ## Original way
-    with urllib.request.urlopen(url) as img_url:
-        res = img_url.read()
+def load_image(img_src):
+    """Open image bytes from a URL/file and return as a numpy array."""
+    logging.info("predict.py: Inside load_image")
+
+    isStr = isinstance(img_src, str)
+    res = img_src
+
+    if isStr:
+        with urllib.request.urlopen(img_src) as img_url:
+            res = img_url.read()
     
     img = Image.open(BytesIO(res)).resize((128, 128))
-    # img = img.convert("L")  # convert the image to grayscale.
-
-    ## Attempts to load using keras as opposed to PIL
-    # with open(BytesIO(res), 'rb') as f: # Convert from file to bytes
-    #     contents = f.read()
-
-    # resp = requests.get(url)
-    # b = BytesIO(resp.content)
-    # img = image.load_img(contents)
-    # with urllib.request.urlopen(url) as img_url:
-    #     img = image.load_img(BytesIO(img_url.read()), target_size=(128, 128))
 
     return image.img_to_array(img)
 
-def load_image_b(img_file):
-    
-    img = Image.open(BytesIO(img_file)).resize((128, 128))
-    # img = img.convert("L")
-
-    return image.img_to_array(img)
-
-def predict_image_from_url(image_url):
-    """Read an image from a URL, """
-    logging.info("predict.py: Image URL received: %s", image_url)
-    ### Conversions
-    # Convert from url to np.array
-    img = load_image(image_url)
-
-    # Rescaling pixels
-    img = img / 255.0
-
-    # Convert from shape (128, 128, 3) to (1, 128, 128, 3)
-    img = tf.expand_dims(img, axis=0)
-    logging.info(img.shape.as_list())
-
-    ### Load the model file
-    function_dir = os.path.dirname(os.path.realpath(__file__))
-    model_path = os.path.join(function_dir, "best-model.hdf5")
-    model = load_model(model_path)
-
-    ### Send the image through the model
-    prediction = model.predict(img)
-
-    return prediction
-
-def predict_image_from_file(image_file):
+def predict_image(image_file):
     """Read an image from a bytestream, """
+    isStr = isinstance(image_file, str)
+    if isStr:
+        logging.info("predict.py: Transferred a URL: %s", isStr)
+    else:
+        logging.info("predict.py: Transferred a file")
+    
     ### Conversions
     # Convert from bytes to np.array
-    img = load_image_b(image_file)
+    img = load_image(image_file)
 
     # Rescaling pixels
     img = img / 255.0
@@ -79,8 +43,6 @@ def predict_image_from_file(image_file):
     # Convert from shape (128, 128, 3) to (1, 128, 128, 3)
     img = tf.expand_dims(img, axis=0)
     print(img.shape.as_list())
-
-
 
     ### Load the model file
     function_dir = os.path.dirname(os.path.realpath(__file__))
